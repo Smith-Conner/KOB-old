41eb2e1170be2301a57fa077f0c647a6705f8007
Conner Smith <jjsmithcs1@gmail.com>
2020-12-20 02:36:37 -0500

    Quality of life Changes and code Clean up

--- Kobold/MainWindow.xaml.cs
+++ Kobold/MainWindow.xaml.cs
@@ -28,32 +28,67 @@ namespace Kobold
             InitializeComponent();
             BeginOneDotONE();
         }
-
-        public void BeginOneDotONE()
+        private void xSubmit_PreviewKeyDown(object sender, KeyEventArgs e)
         {
-            //xTextAdventureOutput.Text = "Hello, I have been made to give you a fun and exiting new experiance when it comes to text adventures. I want to be the perfect and most well made AI that generates dungeons for the player to explore givin their wants and needs. \n\n To begin please state that you would like to begin the text adventure and the number of levels that you would like in it.";
-
+            if (e.Key == Key.Return)
+            {
+                PlayerSubmitTA = xTextAdventureUserImput.Text.ToLower();
+                Keys();
+            }
         }
-        
-        
-        public void method1()
-        {
-
-        }
-        public void method2()
-        {
-
-        }
-
-
-        
         private void xSubmit_Click(object sender, RoutedEventArgs e)
         {
-            PlayerSubmitTA = xTextAdventureUserImput.Text;
+            PlayerSubmitTA = xTextAdventureUserImput.Text.ToLower();
+            Keys();
+        }
+        private void xClear_Click(object sender, RoutedEventArgs e)
+        {
+            xTextAdventureUserImput.Text = "";
         }
 
-        private void xNext_Click(object sender, RoutedEventArgs e)
+
+        //The very Begining of the Text Adventure
+        public void BeginOneDotONE()
         {
+            xTextAdventureOutput.Text = "Why hello there im sure you know this but you are running KOBold. The best Text Adventure game to have ever been made. Or at least it will be the best shortly. For now I would need you to learn how to use me. Im sure at some point there will be a user manual for me, but for now lets start simple. \n\n " +
+                "For starters I am very basic in function and will improve as more users write on my flaws. Please ask me to start the dungeon.";
         }
+        public void BeginOneDotTwo()
+        {
+            xTextAdventureOutput.Text = "Great, now all that you need to do is pick the number of events you want in your adventure. Of course this effects how long you will spend playing this game. So please start by saying how many events you want. Using full sentances will of course help.\n\n " +
+                "Please know that at this time there is only 1 event so try not to go over that much.";
+        }
+        public void BeginOneDotThree()
+        {
+            xTextAdventureOutput.Text = "See isn't this fun. Already picking the stuff you want to do. Do know that it's going to get more free in your choices";
+        }
+
+
+
+
+        // TODO Create a dungeon event
+        #region //Dungeon Events
+
+        #endregion
+
+        #region //Key Word finder
+        public void Keys()
+        {
+            if ((PlayerSubmitTA.Contains("start") || PlayerSubmitTA.Contains("begin")) && (PlayerSubmitTA.Contains("dungeon") || PlayerSubmitTA.Contains("adventure")))
+            {
+                //Starts the dungeon
+                BeginOneDotTwo();
+            }else if ((PlayerSubmitTA.Contains("1")|| PlayerSubmitTA.Contains("one")) && (PlayerSubmitTA.Contains("event") || PlayerSubmitTA.Contains("floor") || PlayerSubmitTA.Contains("encounter")))
+            {
+                BeginOneDotThree();
+            }
+        }
+        #endregion
+
+
+
+
+
+
     }
 }
